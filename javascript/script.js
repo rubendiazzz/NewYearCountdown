f7d17a74b398faea66173f916bfcc08f9e30f12d
Rubén Díaz Fraga <89214621+rubendiazzz@users.noreply.github.com>
2023-03-14 19:55:28 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,22 @@
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minsEl =  document.getElementById('mins');
-const secondsEl = document.getElementById('seconds');
-
-const newYears = '1 Jan 2022';
-
-function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
-
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds /3600) % 24;
-    const mins = Math.floor(totalSeconds /60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
-
-    daysEl.innerHTML = formatTime(days);
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
-}
-
-function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-}
-
 // initial call
 countdown();
 
 setInterval(countdown, 1000);
+
+const currentYear = new Date().getFullYear();
+const newYears = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function countdown() {
+  const now = new Date().getTime();
+  const distance = newYears - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  console.log(`Countdown to New Year's Day ${currentYear + 1}: ${days}d ${hours}h ${minutes}m ${seconds}s`);
+}
+
+countdown();
 
